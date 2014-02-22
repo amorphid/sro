d87f34f0c3e5ae22e81b96e84ff842557c4fa258
Michael Pope <amorphid@captaincoder.io>
2014-02-21 23:39:26 -0800

    Refactor regex in Sro::Uuid#pattern to be shorter

--- lib/sro/uuid.rb
+++ lib/sro/uuid.rb
@@ -1,6 +1,7 @@
 module Sro::Uuid
   def self.pattern
-    /([a-z]|\d){8}-([a-z]|\d){4}-([a-z]|\d){4}-([a-z]|\d){4}-([a-z]|\d){12}/
+    p = /([a-z]|\d)/
+    /#{p}{8}-(#{p}{4}-){3}#{p}{12}/
   end
 
   def self.version5
