e1615478f863cd7b89006fe4cb1bc4e2284fb611
Michael Pope <amorphid@captaincoder.io>
2014-02-21 19:47:32 -0800

    (red) Write failing test => Sro::Uuid::Version5#mac_address retrieves computers mac address

--- spec/sro/uuid/version5_spec.rb
+++ spec/sro/uuid/version5_spec.rb
@@ -3,6 +3,14 @@ require "spec_helper"
 describe Uuid::Version5 do
   subject { Uuid::Version5.new }
 
+  context "#mac_address" do
+    it "retrieves computers mac address" do
+      mac_address = subject.mac_address
+      pattern     = /((\d|[a-f]){2}:){5}(\d|[a-f]){2}/
+      expect(mac_address).to match(pattern)
+    end
+  end
+
   context "#timestamp" do
     it "returns a UUIDTools::UUID object" do
       object    = subject.timestamp
