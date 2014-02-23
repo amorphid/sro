74d833575b96e6c19ab49b4a201c005c115543b1
Michael Pope <amorphid@captaincoder.io>
2014-02-22 18:26:34 -0800

    (red) Write failing test => Sro::Mock::ActiveRecordModel inherits from ActiveRecord::Base

--- spec/sro/mock/active_record_model_spec.rb
+++ spec/sro/mock/active_record_model_spec.rb
@@ -3,6 +3,12 @@ require "spec_helper"
 describe Sro::Mock::ActiveRecordModel do
   subject { Sro::Mock::ActiveRecordModel.new }
 
+  it "inherits from ActiveRecord::Base" do
+    klass    = subject.class
+    ancestors = klass
+    expect(ancestors).to include(ActiveRecord::Base)
+  end
+
   context "#active_record_model" do
     it "returns an ActiveRecordModel object" do
       object = subject.active_record_model
