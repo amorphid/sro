0ae2f1591c744472dd191b2fe7aa39906a6d79eb
Michael Pope <amorphid@captaincoder.io>
2014-02-22 22:52:21 -0800

    Add inject_getter method to ActiveRecordModel class

--- lib/sro/mock/active_record_model.rb
+++ lib/sro/mock/active_record_model.rb
@@ -3,5 +3,10 @@ module Sro::Mock
     def active_record_model
       ActiveRecordModel.new
     end
+
+    def inject_getter(key)
+      getter = set_getter(key)
+      self.instance_eval(getter)
+    end
   end
 end
