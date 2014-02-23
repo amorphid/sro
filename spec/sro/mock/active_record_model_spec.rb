d47ceb386463f002d954fb4cd57ee4cea6690108
Michael Pope <amorphid@captaincoder.io>
2014-02-22 17:06:22 -0800

    (red) Write failing test => Sro::Mock::ActiveRecordModel#active_record_model returns an ActiveRecordModel object

---
+++ spec/sro/mock/active_record_model_spec.rb
@@ -0,0 +1,12 @@
+require "spec_helper"
+
+describe Sro::Mock::ActiveRecordModel do
+  subject { Sro::Mock::ActiveRecordModel.new }
+
+  context "#active_record_model" do
+    it "returns an ActiveRecordModel object" do
+      object = subject.active_record_model
+      expect(object).to be_instance_of(ActiveRecordModel)
+    end
+  end
+end
